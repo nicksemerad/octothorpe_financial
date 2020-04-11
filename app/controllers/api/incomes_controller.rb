c49a83edd720ab68cb62ee38218f02b216817405
Nick Semerad <nicksemerad@gmail.com>
2020-04-11 11:36:42 -0600

    incomes controller

--- app/controllers/api/incomes_controller.rb
+++ app/controllers/api/incomes_controller.rb
@@ -1,13 +1,44 @@
 class Api::IncomesController < ApplicationController
-  def index
-  end
 
-  def show
-  end
+  before_action :set_budget
 
-  def create
-  end
+    def index
+      render json: @budget.incomes
+    end
 
-  def update
+    def show
+      @income = @budget.incomes.find(parmam[:id])
+      render json: @income
+    end
+
+    def create
+      @income = @budget.incomes.new(income_params)
+      if @income.save
+        render json: @income
+      else
+        render json: { errors: @income.errors}, status: :unprocessable_entity
+    end
+
+    def update
+      @income = @budget.incomes.find(params[:id])
+      if @income.update(income_params)
+        render json: @income
+      else
+        render json: { errors: @income.errors}, status: :unprocessable_entity
+    end
+
+    def destroy
+      Income.find(params[:id]).destroy
+      render json: { message: "Income Deleted" }
+    end
+
+    private
+
+    def income_params
+      params.require(:income).permit
+      (:payerName, :category, :amount, :freq, :nextPaydate)
+
+    def set_budget
+      @budget = Budget.find(params[:budget_id])
+    end
   end
-end

61fce4a404e4a1338f90d0386bc64df57602df34
Diane <dianenguyen9521175@gmail.com>
2020-04-11 14:44:57 -0600

    pull

--- client/src/index.js
+++ client/src/index.js
@@ -6,15 +6,20 @@ import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './providers/AuthProvider';
 import { initMiddleware } from 'devise-axios';
+import IncomeProvider from "./providers/IncomeProvider";
+
+
 
 initMiddleware();
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
+      <IncomeProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </IncomeProvider>
     </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
